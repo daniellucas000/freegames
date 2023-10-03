75624a3366d8530e8369aaffa1a63f2de16e1001
Daniel Lucas <danielluccas000@gmail.com>
2023-10-03 06:56:16 -0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 
-import { ThemeProvider } from 'styled-components';
+// import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from './styles/global';
-import { darkTheme, lightTheme } from './styles/themes';
+// import { darkTheme, lightTheme } from './styles/themes';
 
 import { Outlet } from 'react-router-dom';
 import { Header } from './components/Header';
 
 export function App() {
-  const [theme, setTheme] = useState('light');
+  // const [theme, setTheme] = useState('light');
   return (
-    <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+    // <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+    <>
       <Header />
       <Outlet />
 
       <GlobalStyle />
-    </ThemeProvider>
+    </>
+    // </ThemeProvider>
   );
 }
